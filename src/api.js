c2de2b806cdd2e62a0d45ddbf8e1386ecb1059ef
Samridhilal <samridhilal22@gmail.com>
2024-03-27 21:06:35 +0530

    First Commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '49b310c5bdmshcb5ba1993eaf6c6p1b789cjsn3dde1a0803f7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";  
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "995f9880c90192defc402418fb57210d";
